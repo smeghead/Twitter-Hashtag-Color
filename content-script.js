ce78aa490bf9856da48b90785c7c19e587ab25f4
smeghead <smeghead7@gmail.com>
2011-03-16 00:40:50 +0800

    added style name for Old Twitter. I got a patch from mattn! thx.

--- content-script.js
+++ content-script.js
@@ -4,7 +4,7 @@ var body = document.getElementsByTagName('body')[0];
 body.addEventListener('DOMNodeInserted', function(event){
   if (typeof event.target.querySelectorAll !== 'function')
     return; // maybe text node
-  Array.prototype.slice.call(event.target.querySelectorAll('a.hashtag')).forEach(function(n) {
+  Array.prototype.slice.call(event.target.querySelectorAll('a.hashtag, a.twitter-hashtag')).forEach(function(n) {
     n.style.color = '#FF33FF';
   });
 });
