5ef8439ca654bf4363ca72a352455d17801ba358
mattn <mattn.jp@gmail.com>
2011-03-15 21:52:47 +0900

    fix class name. use querySelectorAll().

--- content-script.js
+++ content-script.js
@@ -2,12 +2,10 @@ console.log('content-script.js: initialize start.');
 
 var body = document.getElementsByTagName('body')[0];
 body.addEventListener('DOMNodeInserted', function(event){
-  var links = event.target.getElementsByTagName('a');
-  for (var i = 0; i < links.length; i++) {
-    var a = links[i];
-    if (a.className.indexOf('twitter-hashtag') > -1) {
-      a.style.color = '#FF33FF';
-    }
-  }
+  if (typeof event.target.querySelectorAll !== 'function')
+    return; // maybe text node
+  Array.prototype.slice.call(event.target.querySelectorAll('a.hashtag')).forEach(function(n) {
+    n.style.color = '#FF33FF';
+  });
 });
 /* vim: set ts=2 sw=2 sts=2 expandtab fenc=utf-8: */
