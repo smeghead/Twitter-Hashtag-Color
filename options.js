3239a034e9a72160c07fc4495adfbfa80f9a50cd
smeghead <smeghead7@gmail.com>
2011-04-27 12:15:37 +0800

    fixed how to save options.

--- options.js
+++ options.js
@@ -1,12 +1,20 @@
+var default_options = {style: 'color: #FF33FF !important;'};
 function get_thc_options() {
   try {
-    var options = {style: 'color: #FF33FF !important;'};
+    var options = default_options;
+    //console.log(options);
+    console.log('y');
+    console.log(localStorage.thc_options);
     if (localStorage.thc_options) {
+    console.log('yy');
       options = JSON.parse(localStorage.thc_options);
     }
+    console.log('z');
   
     //save
     save_thc_options(options);
+    console.log('x');
+    console.log(options);
     return options;
   } catch (e) {
     console.log(e);
@@ -16,5 +24,5 @@ function save_thc_options(options) {
   localStorage['thc_options'] = JSON.stringify(options);
 }
 function clear_thc_options() {
-  localStorage['thc_options'] = {};
+  localStorage['thc_options'] = JSON.stringify(default_options);
 }
